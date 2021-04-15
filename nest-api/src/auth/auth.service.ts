9dbdc8816018e3e05229e5d8a5f37dc1b6badfba
David Schwarb <dschwarb@architectnow.net>
2021-04-15 17:50:25 -0500

    updated create user dto

--- nest-api/src/auth/auth.service.ts
+++ nest-api/src/auth/auth.service.ts
@@ -13,6 +13,12 @@ export class AuthService {
     if (!user) {
       return false;
     }
+    // Save status of user as active
+    // Return {userId: sadsadasda, authenticated: boolean}
+    // -- Front end --
+    // 1.  Grab the current list of authenticated users from session storage
+    // 2. Add this response to the list.
+    // 3. Save to session storage.  [ { userId: david, authenticated: boolean } , {userId: keith, authenticated: boolean }     ]
     return bcrypt.compareSync(loginVm.password, user.passwordHash);
   }
 }
