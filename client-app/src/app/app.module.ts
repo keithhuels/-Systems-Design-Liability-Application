35b695058b9e75fe00dead684630c78da1c978df
gmmhb <keith.huels@bayer.com>
2021-04-16 09:47:06 -0500

    added angular material and now hiding password text

--- client-app/src/app/app.module.ts
+++ client-app/src/app/app.module.ts
@@ -1,23 +1,24 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {AdminloginComponent} from './components/dashboard/adminlogin/adminlogin.component';
-import {AdminlogoutComponent} from './components/dashboard/adminlogout/adminlogout.component';
-import {AdminlookupComponent} from './components/dashboard/adminlookup/adminlookup.component';
-import {DashboardHeaderComponent} from './components/dashboard/dashboard-header/dashboard-header.component';
-import {DashboardComponent} from './components/dashboard/dashboard.component';
-import {DefaultDashboardComponent} from './components/dashboard/default-dashboard/default-dashboard.component.';
-import {ModalComponent} from './components/dashboard/modal/modal.component';
-import {SigninComponent} from './components/dashboard/signin/signin.component';
-import {SignoutComponent} from './components/dashboard/signout/signout.component';
-import {SignupComponent} from './components/dashboard/signup/signup.component';
-import {UserListComponent} from './components/dashboard/user-list/user-list.component';
-import {LandingPageComponent} from './components/landing-page/landing-page.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {ApiInterceptor} from './interceptors/api.interceptor';
-import {ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { MatSliderModule } from "@angular/material/slider";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { AdminloginComponent } from "./components/dashboard/adminlogin/adminlogin.component";
+import { AdminlogoutComponent } from "./components/dashboard/adminlogout/adminlogout.component";
+import { AdminlookupComponent } from "./components/dashboard/adminlookup/adminlookup.component";
+import { DashboardHeaderComponent } from "./components/dashboard/dashboard-header/dashboard-header.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { DefaultDashboardComponent } from "./components/dashboard/default-dashboard/default-dashboard.component.";
+import { ModalComponent } from "./components/dashboard/modal/modal.component";
+import { SigninComponent } from "./components/dashboard/signin/signin.component";
+import { SignoutComponent } from "./components/dashboard/signout/signout.component";
+import { SignupComponent } from "./components/dashboard/signup/signup.component";
+import { UserListComponent } from "./components/dashboard/user-list/user-list.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
+import { ApiInterceptor } from "./interceptors/api.interceptor";
 
 @NgModule({
   declarations: [
@@ -35,13 +36,21 @@ import {ReactiveFormsModule} from '@angular/forms';
     AdminlogoutComponent,
     ModalComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, HttpClientModule, ReactiveFormsModule],
-  providers: [{
-    provide: HTTP_INTERCEPTORS,
-    useClass: ApiInterceptor,
-    multi: true
-  }],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
