4250a928bdf57e8b06a25f61a7882b9f91876747
gmmhb <keith.huels@bayer.com>
2021-05-04 13:51:34 -0500

    added password validation on create account forms

--- client-app/src/app/components/dashboard/signup/signup.component.ts
+++ client-app/src/app/components/dashboard/signup/signup.component.ts
@@ -1,9 +1,15 @@
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
 import { Router } from "@angular/router";
 import { ModalService } from "../modal/modal.service";
 import { User } from "../user-list/user-list.component";
+import { PasswordValidationService } from "./../../../../password-validation.service";
 import { UsersService } from "./../../shared/users.service";
 
 @Component({
@@ -16,24 +22,35 @@ export class SignupComponent implements OnInit {
     private readonly router: Router,
     private readonly modalService: ModalService,
     private readonly http: HttpClient,
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly passwordValidator: PasswordValidationService,
+    private formBuilder: FormBuilder
   ) {}
 
   form: FormGroup;
 
   ngOnInit(): void {
-    this.form = new FormGroup({
-      username: new FormControl("", [Validators.required]),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      password: new FormControl("", [Validators.required]),
-      firstName: new FormControl("", [Validators.required]),
-      lastName: new FormControl("", [Validators.required]),
-      organization: new FormControl("", [Validators.required]),
-      waiverAccepted: new FormControl(false, [
-        Validators.required,
-        Validators.requiredTrue,
-      ]),
-    });
+    this.form = this.formBuilder.group(
+      {
+        username: new FormControl("", [Validators.required]),
+        email: new FormControl("", [Validators.required, Validators.email]),
+        password: new FormControl("", [Validators.required]),
+        confirmPassword: new FormControl("", [Validators.required]),
+        firstName: new FormControl("", [Validators.required]),
+        lastName: new FormControl("", [Validators.required]),
+        organization: new FormControl("", [Validators.required]),
+        waiverAccepted: new FormControl(false, [
+          Validators.required,
+          Validators.requiredTrue,
+        ]),
+      },
+      {
+        validator: this.passwordValidator.passwordMatchValidator(
+          "password",
+          "confirmPassword"
+        ),
+      }
+    );
   }
 
   onBackClick() {
